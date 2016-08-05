1c2a10af519f2d7b234d3dab8ecb48da1a32cab3
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-08-04 23:23:34 -0400

    added update functionality

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -11,15 +11,29 @@ class PlaylistsController < ApplicationController
     @playlist = Playlist.new
   end
 
+  def edit
+    @playlist = Playlist.find(params[:id])
+  end
+
   def create
     @playlist = Playlist.new(playlist_params)
 
     if @playlist.save
-    redirect_to @playlist
-  else
-    render 'new'
+      redirect_to @playlist
+    else
+      render 'new'
+    end
   end
-end
+
+  def update
+    @playlist = Playlist.find(params[:id])
+
+    if @playlist.update(playlist_params)
+      redirect_to @playlist
+    else
+      render 'edit'
+    end
+  end
 
   private
   def playlist_params
